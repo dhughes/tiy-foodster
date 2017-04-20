57291139f0fb71388e42ad58bb2744bdbd659167
lexinamer <lexinamer@gmail.com>
2017-04-20 16:27:43 -0400

    fixing js bug

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,16 +1,17 @@
 const moreItems = document.querySelectorAll('.more');
 
 for(var i=0; i<moreItems.length; i++){
-  let li = document.createElement('li');
 
   moreItems[i].addEventListener('click', (e) => {
     if (e.target.getAttribute("aria-label") === "add-ingrdient"){
+      let li = document.createElement('li');
       let input = document.querySelector('ul input');
       let newItem = input.cloneNode(true);
       newItem.value = "";
       document.querySelector('ul').appendChild(li).appendChild(newItem);
 
     } else if (e.target.getAttribute("aria-label") === "add-instruction"){
+      let li = document.createElement('li');
       let input = document.querySelector('ol input');
       let newItem = input.cloneNode(true);
       newItem.value = "";
