5c6624a9efacca70956eb4a6f014728b610bffeb
lexinamer <lexinamer@gmail.com>
2017-04-20 12:56:01 -0400

    refactored js for recipe page

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,20 @@
-const addIngredient = document.getElementById('add-ingredient');
-const addInstruction = document.getElementById('add-instruction');
+const moreItems = document.querySelectorAll('.more');
 
-
-addIngredient.addEventListener('click', () => {
+for(var i=0; i<moreItems.length; i++){
   let li = document.createElement('li');
-  let input = document.querySelector('ul input');
-  let newItem = input.cloneNode(true);
-  newItem.value = "";
 
- document.querySelector('ul').appendChild(li).appendChild(newItem);
-});
+  moreItems[i].addEventListener('click', (e) => {
+    if (e.target.getAttribute("aria-label") === "add-ingrdient"){
+      let input = document.querySelector('ul input');
+      let newItem = input.cloneNode(true);
+      newItem.value = "";
+      document.querySelector('ul').appendChild(li).appendChild(newItem);
 
-addInstruction.addEventListener('click', () => {
-  let li = document.createElement('li');
-  let input = document.querySelector('ol input');
-  let newItem = input.cloneNode(true);
-  newItem.value = "";
-
- document.querySelector('ol').appendChild(li).appendChild(newItem);
-});
+    } else if (e.target.getAttribute("aria-label") === "add-instruction"){
+      let input = document.querySelector('ol input');
+      let newItem = input.cloneNode(true);
+      newItem.value = "";
+      document.querySelector('ol').appendChild(li).appendChild(newItem);
+    }
+  })
+};
