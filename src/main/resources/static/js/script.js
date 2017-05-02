31737c3d9b78e9212326a89e55978d739e55cd9e
lexinamer <lexinamer@gmail.com>
2017-05-02 11:54:34 -0400

    fixing form fields. change from input to txtarea

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,18 +5,18 @@ for (var i = 0; i < moreItems.length; i++) {
     moreItems[i].addEventListener('click', (e) => {
         if (e.target.getAttribute("aria-label") === "add-ingrdient") {
             let li = document.createElement('li');
-            let input = document.querySelector('ul input');
+            let input = document.querySelector('ul textarea');
             let newItem = input.cloneNode(true);
             newItem.value = "";
-            newItem.name = newItem.name.replace(/(.*\[)(.*?)(\].*)/, "$1" + document.querySelectorAll("ul input").length + "$3");
+            newItem.name = newItem.name.replace(/(.*\[)(.*?)(\].*)/, "$1" + document.querySelectorAll("ul textarea").length + "$3");
             document.querySelector('ul').appendChild(li).appendChild(newItem);
 
         } else if (e.target.getAttribute("aria-label") === "add-instruction") {
             let li = document.createElement('li');
-            let input = document.querySelector('ol input');
+            let input = document.querySelector('ol textarea');
             let newItem = input.cloneNode(true);
             newItem.value = "";
-            newItem.name = newItem.name.replace(/(.*\[)(.*?)(\].*)/, "$1" + document.querySelectorAll("ol input").length + "$3");
+            newItem.name = newItem.name.replace(/(.*\[)(.*?)(\].*)/, "$1" + document.querySelectorAll("ol textarea").length + "$3");
             document.querySelector('ol').appendChild(li).appendChild(newItem);
         }
     })
